bc1d707fa6ffe6c57e73eb4483308b932a0c1dfc
MidasVanVeen <midasvanveen.email@gmail.com>
2022-04-23 00:04:43 +0200

    .

---
+++ index.js
@@ -0,0 +1,12 @@
+var projects_request = new Request('https://api.github.com/users/MidasVanVeen/starred?per_page=100');
+
+const div = document.getElementById("projects_div");
+
+fetch(projects_request).then(resp => {
+	resp.json().then(data => {
+		for (let i = 0; i < data.length; i++) {
+			projects_div.innerHTML += '<div class="project_item"><h1>' + data[i]['name'] + '</h1><p>' + data[i]['description'] + '</p></div>';
+		}
+	})
+})
+
