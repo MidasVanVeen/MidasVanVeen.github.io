111c703d35cda8df78e31d435d51171b4868ab96
MidasVanVeen <midasvanveen.email@gmail.com>
2022-04-30 20:29:23 +0200

    .

--- js/mode_switch.js
+++ js/mode_switch.js
@@ -1,13 +1,18 @@
 "use strict";
-var mode = 'light';
+var mode = "dark";
+if (getCookie("mode") === "light") {
+    mode = "light";
+}
 function toggleMode() {
     if (mode === 'light') {
         setDarkMode();
         mode = 'dark';
+        setCookie("mode", "dark", 30);
     }
     else if (mode === 'dark') {
         setLightMode();
         mode = 'light';
+        setCookie("mode", "light", 30);
     }
 }
 function setDarkMode() {
@@ -25,4 +30,24 @@ function setLightMode() {
     if (msg != null) {
         msg.innerHTML = 'Click me for dark mode';
     }
+}
+function getCookie(cname) {
+    let name = cname + "=";
+    let ca = document.cookie.split(';');
+    for (let i = 0; i < ca.length; i++) {
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+function setCookie(cname, cvalue, exdays) {
+    const d = new Date();
+    d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+    let expires = "expires=" + d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
--- ts/mode_switch.ts
+++ ts/mode_switch.ts
@@ -1,13 +1,20 @@
-var mode = 'light';
 type mode = "light" | "dark";
+
+var mode: mode = "dark";
+setDarkMode();
+if (getCookie("mode") === "light") {
+	mode = "light";
+}
 
 function toggleMode() {
 	if (mode === 'light') {
 		setDarkMode();
 		mode = 'dark';
+		setCookie("mode", "dark", 30);
 	} else if (mode === 'dark') {
 		setLightMode();
 		mode = 'light';
+		setCookie("mode", "light", 30);
 	}
 }
 
@@ -27,4 +34,26 @@ function setLightMode() {
 	if (msg != null) {
 		msg.innerHTML = 'Click me for dark mode';
 	}
+}
+
+function getCookie(cname: string) {
+	let name = cname + "=";
+	let ca = document.cookie.split(';');
+	for(let i = 0; i < ca.length; i++) {
+		let c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+		  return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
+
+function setCookie(cname: string, cvalue: string, exdays: number) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+  let expires = "expires="+d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
 }
