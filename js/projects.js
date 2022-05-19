a28367d3ade2e5854aa36ce39882b4b113b152f2
Midas van Veen <midasvanveen.email@gmail.com>
2022-05-19 20:14:21 +0200

    .

--- js/projects.js
+++ js/projects.js
@@ -1,13 +1,22 @@
 "use strict";
 var projects_request = new Request('https://api.github.com/users/MidasVanVeen/repos');
+var colab_projects_request = new Request('https://api.github.com/users/Pix4Devs/repos');
 const projects_div = document.getElementById("projects_div");
-var fetched_data = [];
+const colab_projects_div = document.getElementById("colab_projects_div");
 fetch(projects_request).then(resp => {
     resp.json().then(data => {
-        fetched_data = data;
         for (let i = 0; i < data.length; i++) {
             if (projects_div != null) {
                 projects_div.innerHTML += '<div onclick="window.location.href = \'' + data[i]['html_url'] + '\'" class="project_item"><h1>' + data[i]['name'] + '</h1><p>' + data[i]['description'] + '</p></div>';
+            }
+        }
+    });
+});
+fetch(colab_projects_request).then(resp => {
+    resp.json().then(data => {
+        for (let i = 0; i < data.length; i++) {
+            if (colab_projects_div != null) {
+                colab_projects_div.innerHTML += '<div onclick="window.location.href = \'' + data[i]['html_url'] + '\'" class="project_item"><h1>' + data[i]['name'] + '</h1><p>' + data[i]['description'] + '</p></div>';
             }
         }
     });
