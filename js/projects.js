ab0ea6cdd6759b21b98183a0c84b28763bdd7c96
Midas van Veen <midasvanveen.email@gmail.com>
2022-05-19 17:48:58 +0200

    .

--- js/projects.js
+++ js/projects.js
@@ -1,10 +1,5 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const remarkable_min_js_1 = __importDefault(require("./remarkable.min.js"));
-var projects_request = new Request('https://api.github.com/users/MidasVanVeen/starred?per_page=100');
+var projects_request = new Request('https://api.github.com/users/MidasVanVeen/repos');
 const projects_div = document.getElementById("projects_div");
 var fetched_data = [];
 fetch(projects_request).then(resp => {
@@ -12,25 +7,8 @@ fetch(projects_request).then(resp => {
         fetched_data = data;
         for (let i = 0; i < data.length; i++) {
             if (projects_div != null) {
-                projects_div.innerHTML += '<div onclick="maximize(\"' + data['name'] + '\"" class="project_item"><h1>' + data[i]['name'] + '</h1><p>' + data[i]['description'] + '</p></div>';
+                projects_div.innerHTML += '<div onclick="window.location.href = \'' + data[i]['html_url'] + '\'" class="project_item"><h1>' + data[i]['name'] + '</h1><p>' + data[i]['description'] + '</p></div>';
             }
         }
     });
 });
-function maximize(project_name) {
-    if (fetched_data != []) {
-        fetched_data.forEach(element => {
-            if (element['name'] == project_name) {
-                const md = new remarkable_min_js_1.default();
-                fetch(new Request("https://raw.githubusercontent.com/MidasVanVeen/" + project_name + "/main/README.md")).then(resp => {
-                    resp.text().then(READMEstring => {
-                        let projects_maxi_div = document.getElementById("projects_maxi");
-                        if (projects_maxi_div != null) {
-                            projects_maxi_div.innerHTML = md.render(READMEstring);
-                        }
-                    });
-                });
-            }
-        });
-    }
-}
--- ts/projects.ts
+++ ts/projects.ts
@@ -1,6 +1,4 @@
-import Remarkable from './remarkable.min.js';
-
-var projects_request: any = new Request('https://api.github.com/users/MidasVanVeen/starred?per_page=100');
+var projects_request: any = new Request('https://api.github.com/users/MidasVanVeen/repos');
 
 const projects_div = document.getElementById("projects_div");
 
@@ -11,28 +9,8 @@ fetch(projects_request).then(resp => {
 		fetched_data = data;
 		for (let i = 0; i < data.length; i++) {
 			if (projects_div != null) {
-				projects_div.innerHTML += '<div onclick="maximize(\"' + data['name'] + '\"" class="project_item"><h1>' + data[i]['name'] + '</h1><p>' + data[i]['description'] + '</p></div>';
+				projects_div.innerHTML += '<div onclick="window.location.href = \'' + data[i]['html_url'] + '\'" class="project_item"><h1>' + data[i]['name'] + '</h1><p>' + data[i]['description'] + '</p></div>';
 			}
 		}
 	});
 });
-
-function maximize(project_name: String) {
-	if (fetched_data != []) {
-		fetched_data.forEach(element => {
-			if (element['name'] == project_name) {
-
-				const md = new Remarkable();
-				
-				fetch(new Request("https://raw.githubusercontent.com/MidasVanVeen/" + project_name + "/main/README.md")).then(resp => {
-					resp.text().then(READMEstring => {
-						let projects_maxi_div = document.getElementById("projects_maxi");
-						if (projects_maxi_div != null) {
-							projects_maxi_div.innerHTML = md.render(READMEstring);
-						}
-					})
-				});
-			}
-		});
-	}
-}
